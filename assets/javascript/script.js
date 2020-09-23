d2225d5525237e3e1cfd8fea8f96d736b83d32ac
lazaro guzman <jorrel724@gmai.com>
2020-09-23 13:56:37 -0400

    cleaned up carasol and got it to autoplay

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,3 +9,7 @@ var instance = M.Carousel.init({
     fullWidth: true,
     indicators: true
   });
+  $('.carousel').carousel();
+  setInterval(function() {
+    $('.carousel').carousel('next');
+  }, 5000); // every 2 seconds
