3a77fee3186edc68919e93625760ccbe950f77b5
lazaro guzman <jorrel724@gmai.com>
2020-09-23 13:57:37 -0400

    added some notes to the autoplay javascrip

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,7 +9,8 @@ var instance = M.Carousel.init({
     fullWidth: true,
     indicators: true
   });
+  //auto play here and its in millaseconds
   $('.carousel').carousel();
   setInterval(function() {
     $('.carousel').carousel('next');
-  }, 5000); // every 2 seconds
+  }, 5000); // every 5 seconds
