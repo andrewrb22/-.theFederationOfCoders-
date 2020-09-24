1126f80090a4a22bb18dac13896c1e60946c837e
Christian Marquez <christianmarquez41@gmail.com>
2020-09-24 11:57:45 -0400

    added apikey to js and got a response from food api

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,12 +4,27 @@ $('.carousel.carousel-slider').carousel({
   indicators: true
 });
 
-$(".btn").on("click", function(){
+$(".btn").on("click", function () {
   //testing the buttons
   alert("you clicked ");
 })
-  //auto play here and its in millaseconds
-  $('.carousel').carousel();
-  setInterval(function() {
-    $('.carousel').carousel('next');
-  }, 5000); // every 5 seconds
+//auto play here and its in millaseconds
+$('.carousel').carousel();
+setInterval(function () {
+  $('.carousel').carousel('next');
+}, 5000); // every 5 seconds
+
+var myAPI = "344c39f083fc4d8dac4a76e6e15bd196"
+
+var queryURL = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + myAPI;
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+})
+
+  .then(function (response) {
+
+    console.log(response);
+
+  })
