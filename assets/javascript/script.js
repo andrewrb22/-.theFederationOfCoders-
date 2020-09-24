1bca936aec86b867f19d1935635654b0be81f595
Andrew Rodriguez <andrewrb22@gmail.com>
2020-09-24 12:08:48 -0400

    requesting wine information and console.log

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -13,3 +13,10 @@ $(".btn").on("click", function(){
   setInterval(function() {
     $('.carousel').carousel('next');
   }, 5000); // every 5 seconds
+// request wine information/ steak pairing
+  $.ajax({
+url : "https://api.spoonacular.com/food/wine/pairing?food=steak&apiKey=3be041b3f9c84afaa2bb5ee16a7b4c01",
+method : "GET"
+  }).then(function wine(wineEl) {
+   console.log(wineEl);
+  })
