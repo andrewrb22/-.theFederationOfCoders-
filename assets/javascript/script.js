f03441470894c43af548fa0648897f84b08dee97
Carlito Algarin <19lito96@gmail.com>
2020-09-25 12:00:13 -0400

    .

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,7 @@
 // carousel code from materialize
-$('.carousel.carousel-slider').carousel({
+$(".carousel.carousel-slider").carousel({
   fullWidth: true,
-  indicators: true
+  indicators: true,
 });
 
 $(".btn").on("click", function () {
@@ -10,38 +10,38 @@ $(".btn").on("click", function () {
   console.log(food);
   winePairing(food);
   foodRecipe(food);
-})
-
+});
 
 //auto play here and its in millaseconds
-$('.carousel').carousel();
+$(".carousel").carousel();
 setInterval(function () {
-  $('.carousel').carousel('next');
+  $(".carousel").carousel("next");
 }, 5000); // every 5 seconds
 
-
-
-function foodRecipe(food){
-$.ajax({
-  url: "https://api.spoonacular.com/recipes/complexSearch?query="  + food +  "&apiKey=344c39f083fc4d8dac4a76e6e15bd196",
-  method: "GET"
-})
-
-  .then(function (response) {
-
-    console.log(response);
-
+function foodRecipe(food) {
+  $.ajax({
+    url:
+      "https://api.spoonacular.com/recipes/complexSearch?query=" +
+      food +
+      "&apiKey=344c39f083fc4d8dac4a76e6e15bd196",
+    method: "GET",
   })
+  .then(function (response) {
+    console.log(response);
+  });
 }
 
 function winePairing(food) {
   // request wine information/ steak pairing
   $.ajax({
-    url: "https://api.spoonacular.com/food/wine/pairing?food=" + food + "&apiKey=3be041b3f9c84afaa2bb5ee16a7b4c01",
-    method: "GET"
+    url:
+      "https://api.spoonacular.com/food/wine/pairing?food=" +
+      food +
+      "&apiKey=3be041b3f9c84afaa2bb5ee16a7b4c01",
+    method: "GET",
   }).then(function wine(wineEl) {
     console.log(wineEl);
-  })
+  });
 
   function randomnumber(max) {
     return Math.floor(Math.random() * Math.floor(max) + 1);
